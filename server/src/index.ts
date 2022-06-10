2a1e6a849f7198eb6876bc946b4ff62006cf2c7a
Alexis Lopez <alexis.lopez@stgchile.cl>
2022-06-10 10:53:57 -0400

    client

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,8 +15,55 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(responseTime());
 
+const SEND_INTERVAL = 2000;
+
+const donation = {
+  user: 0,
+  amount: 0,
+};
+
+const writeEvent = (res: Response, sseId: string, data: string) => {
+  res.write(`id: ${sseId}\n`);
+  res.write(`data: ${data}\n\n`);
+};
+
+const sendEvent = (_req: Request, res: Response) => {
+  res.writeHead(StatusCodes.OK, {
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+    "Content-Type": "text/event-stream",
+  });
+
+  const sseId = new Date().toDateString();
+
+  setInterval(() => {
+    writeEvent(res, sseId, JSON.stringify(donation));
+  }, SEND_INTERVAL);
+
+  writeEvent(res, sseId, JSON.stringify(donation));
+};
+
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "server side events nodejs 🙂" });
+});
+
+app.get("/dashboard", (req: Request, res: Response) => {
+  if (req.headers.accept === "text/event-stream") {
+    sendEvent(req, res);
+  } else {
+    res.json({ message: "Ok" });
+  }
+});
+
+app.post("/donate", (req, res) => {
+  const amount = req.body.amount || 0;
+
+  if (amount > 0) {
+    donation.amount += amount;
+    donation.user += 1;
+  }
+
+  return res.json({ message: "Thank you 🙏" });
 });
 
 app.all("*", async (req: Request, res: Response) => {
